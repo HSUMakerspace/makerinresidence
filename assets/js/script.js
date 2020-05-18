607b4fff0e719ca151c6a3c3dfdbded687fc3ab1
Tim Miller <twm2@humboldt.edu>
2020-05-18 11:36:49 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,2 +1,7 @@
-  var image = document.getElementsByClassName('parallax-image');
-  new simpleParallax(image);
+window.addEventListener('scroll', function(e){
+  var scrolled = window.pageYOffset;
+  const background = document.querySelector('.background');
+  background.style.top = - (scrolled * 0.8) + 'px';
+  const title = document.querySelector('.title');
+  title.style.top = - (scrolled * 0.3) + 'px';
+});
