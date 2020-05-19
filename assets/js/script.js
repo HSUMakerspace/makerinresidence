6e90253afee929a1e63852863e69884843e348f2
Tim Miller <twm2@humboldt.edu>
2020-05-19 10:18:01 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+$(function() {
+
 window.addEventListener('scroll', function(e){
   var scrolled = window.pageYOffset;
   console.log("scrolled: "+scrolled);
@@ -11,3 +13,19 @@ window.addEventListener('scroll', function(e){
   const title = document.querySelector('.title');
   title.style.top = - (scrolled * -0.8) + 'px';
 });
+
+function addAnchorLink(){
+  $('h3').each(function(i){
+    var sectionId = "section";  
+    sectionId = sectionId+i;
+    $(this).attr('id',sectionId);
+    $(this).wrap('<a href="#'+sectionId+'"></a>');
+    $(this).prepend('<span>:link:</span>');
+  });
+  $('h3').hover(function(){
+    $(this).find('ion-icon').toggle(); 
+  });
+}
+addAnchorLink();
+
+)};
