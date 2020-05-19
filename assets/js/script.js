30e2c3f36aebb905cce3b9f9d8dd01b1902224fb
Tim Miller <twm2@humboldt.edu>
2020-05-18 17:24:46 -0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,11 @@ window.addEventListener('scroll', function(e){
   var scrolled = window.pageYOffset;
   console.log("scrolled: "+scrolled);
   const background = document.querySelector('.background');
-  background.style.top = - ((scrolled * 0.8)+400) + 'px';
+  background.style.top = - (scrolled * -0.3) + 'px';
+  const midground = document.querySelector('.midground');
+  midground.style.top = - (scrolled * 0.1) + 'px';
+  const foreground = document.querySelector('.foreground');
+  foreground.style.top = - (scrolled * 0.8) + 'px';
   
   const title = document.querySelector('.title');
   title.style.top = - (scrolled * -0.8) + 'px';
